7de27e633622cff631c3504dbfbd184f87961693
Peng Li <aqnote@aqnote.com>
2019-11-18 14:43:04 +0800

    format template

--- aqnote.lang/src/main/java/com/aqnote/shared/charset/Charset.java
+++ aqnote.lang/src/main/java/com/aqnote/shared/charset/Charset.java
@@ -7,12 +7,12 @@ package com.aqnote.shared.charset;
 
 /**
  * 类Charset.java的实现描述：TODO 类实现描述
- * 
+ *
  * @author "Peng Li"<aqnote@aqnote.com> Nov 17, 2013 8:07:21 PM
  */
 public enum Charset {
     UTF_8("UTF-8"),
-    GBK("GBK"), 
+    GBK("GBK"),
     GB2312("GB2312");
 
     private String charset;
